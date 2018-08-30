9ce2b009b046db3739e36784a32c3f1885e684fc
Felix Wu <mail@flxwu.com>
2018-08-30 19:28:22 +0200

    add styling for header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,15 +4,31 @@ import styled from 'styled-components';
 const Header = props =>	(
 	<Container>
 		<Title>Looking for Maintainers</Title>
+		<Text>
+			Find projects that are searching for maintainers
+		</Text>
 	</Container>
 );
 
 const Container = styled.header`
 	background-size: 100% 100%;
+	margin-top: 50px;
+	flex: 1;
+	align-items: center;
+	justify-content: center;
 `;
 
 const Title = styled.h1`
 	font-style: italic;
+	text-align: center;
+	font-family: Courier New;
+	flex: 1;
+`;
+
+const Text = styled.p`
+	font-family: Verdana;
+	line-height: 1.8;
+	flex: 1;
 	text-align: center;
 `;
 
--- client/src/components/app.js
+++ client/src/components/app.js
@@ -42,7 +42,7 @@ export default class App extends Component {
 
 const List = styled.ul`
 	display: block;
-	margin-block-start: 1em;
+	margin-block-start: 3em;
 	margin-block-end: 1em;
 	margin-inline-start: 0px;
 	margin-inline-end: 0px;
