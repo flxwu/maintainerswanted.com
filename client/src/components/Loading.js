df02639b749cec20b2f3d9d8b29c4c204fa127e6
Felix Wu <mail@flxwu.com>
2018-08-31 00:41:44 +0200

    add Loading Indicator

---
+++ client/src/components/Loading.js
@@ -0,0 +1,38 @@
+import { h, Component } from 'preact';
+import styled, { keyframes } from 'styled-components';
+
+class Loading extends Component {
+	render () {
+		return (
+			<DotWrapper>
+				<Dot delay="0s" />
+				<Dot delay=".1s" />
+				<Dot delay=".2s" />
+			</DotWrapper>
+		);
+	}
+}
+
+const DotWrapper = styled.div`
+  display: flex;
+  align-items: flex-end;
+`;
+
+const Dot = styled.div`
+  background-color: black;
+  border-radius: 50%;
+  width: 10px;
+  height: 10px;
+  margin: 0 5px;
+  /* Animation */
+  animation: ${BounceAnimation} 0.5s linear infinite;
+  animation-delay: ${props => props.delay};
+`;
+
+const BounceAnimation = keyframes`
+  0% { margin-bottom: 0; }
+  50% { margin-bottom: 15px }
+  100% { margin-bottom: 0 }
+`;
+
+export default Loading;
