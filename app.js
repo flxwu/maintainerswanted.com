01e01ede935cffde4b966ba500f0f05b7c67d199
Quentin Oschatz <quentin@cliffhouse.de>
2018-08-30 21:32:10 +0200

    Semi-Fixed Route error

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({status: 500, error: err});
 });
 
 module.exports = app;
