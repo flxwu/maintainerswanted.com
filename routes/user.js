ca551a2da654b375406486968b79b28ad439f27f
Quentin Oschatz <quentin@cliffhouse.de>
2018-08-30 20:43:41 +0200

    Fixed routes and package.json

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET all gothub projects of user */
 router.get('/ghprojects', function(req, res, next) {
-  res.send("Hello World!");
+
+  // Get projects from github
+  // TODO: Add user param
+  request('http://api.github.com/', function (error, response, body) {
+    console.log('error:', error); // Print the error if one occurred and handle it
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    var projects = response.data;
+  });
+
+  if(projects) {
+    
+  }
+
   next();
 });
 
