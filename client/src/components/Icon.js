44a09616cc791c86d4c76de80a5f3a7a12c9fbcd
Felix Wu <mail@felixwu.dev>
2018-08-30 20:50:51 +0200

    make icon component dynamic accepting FI type

--- client/src/components/Icon.js
+++ client/src/components/Icon.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
+const IconURI = title => `../assets/icons/${title}.svg`;
+
 const Icon = props => (
-	<img src={this.props.src} />
+	<img src={IconURI(props.type)} width={50} height={50} />
 );
 
 export default Icon;
