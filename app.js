501c893e52f14130bd6abb593215153fad5c41b4
Quentin Oschatz <quentin@cliffhouse.de>
2018-08-31 00:30:19 +0200

    Cleanup

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
 
-var indexRouter = require('./routes/index');
 var projectRouter = require('./routes/project');
 
 var app = express();
@@ -20,7 +19,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/api/project', projectRouter);
 
 // catch 404
