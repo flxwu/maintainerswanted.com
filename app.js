bfb450c43bb861fb5adf048591600e9b26d1262c
Felix Wu <mail@felixwu.dev>
2018-08-30 23:36:22 +0200

    delete annoying error handler

--- app.js
+++ app.js
@@ -28,15 +28,4 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.json({status: 500, error: err});
-});
-
 module.exports = app;
