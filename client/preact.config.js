5e83f293cb4b526863ccbf36814a5a17532512d6
Felix Wu <mail@felixwu.dev>
2018-08-31 00:41:27 +0200

    preact dev config

---
+++ client/preact.config.js
@@ -0,0 +1,15 @@
+module.exports = function(config) {
+	config.devServer.proxy = [
+		{
+		// proxy requests matching a pattern:
+			path: '/api/**',
+
+			// where to proxy to:
+			target: 'http://localhost:5000',
+
+			// optionally change Origin: and Host: headers to match target:
+			changeOrigin: true,
+			changeHost: true
+		}
+	];
+};
