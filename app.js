a6d49660db19ee855bbdca9a2bd3b79cb19f1d73
Felix Wu <mail@felixwu.dev>
2018-08-30 21:42:05 +0200

    re-setup backend

--- app.js
+++ app.js
@@ -3,50 +3,29 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var bodyParser = require('body-parser');
-var firebase = require('firebase');
 
-var projectRouter = require('./routes/project');
-var projectListRouter = require('./routes/projectList');
-var userRouter = require('./routes/user');
-
-// Database connection
-var db = require('db.js');
-var database = firebase.database();
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
 var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use('/', indexRouter);
-//app.use('/users', usersRouter);
-
-/*
- * ROUTES:
- *
- * /project/get
- * /project/add
- * /project/update
- * /projectlist
- * /user/ghprojects
- * /user/ghproject/get
- *
-*/
-
-app.use("/project", projectRouter);
-app.use("/projectlist", projectListRouter);
-app.use("/user", userRouter);
-
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
-
-
 
 // error handler
 app.use(function(err, req, res, next) {
@@ -58,7 +37,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
-
 
 module.exports = app;
