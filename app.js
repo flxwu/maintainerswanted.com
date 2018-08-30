e86cec8294d71125a4c67be287b999f8ad2c6ccb
Felix Wu <mail@felixwu.dev>
2018-08-31 01:31:55 +0200

    delete view engine

--- app.js
+++ app.js
@@ -9,15 +9,11 @@ var projectRouter = require('./routes/project');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use('/api/project', projectRouter);
 
