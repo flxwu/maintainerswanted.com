bf527b429c3778755593c09ff091ffb606aeb31b
Felix Wu <mail@flxwu.com>
2018-08-31 02:01:44 +0200

    add async-await plugin for preact build

--- client/preact.config.js
+++ client/preact.config.js
@@ -1,15 +1,21 @@
-module.exports = function(config) {
-	config.devServer.proxy = [
-		{
-		// proxy requests matching a pattern:
-			path: '/api/**',
+// module.exports = function(config) {
+// 	config.devServer.proxy = [
+// 		{
+// 			// proxy requests matching a pattern:
+// 			path: '/api/**',
 
-			// where to proxy to:
-			target: 'http://localhost:5000',
+// 			// where to proxy to:
+// 			target: 'http://localhost:5000',
 
-			// optionally change Origin: and Host: headers to match target:
-			changeOrigin: true,
-			changeHost: true
-		}
-	];
+// 			// optionally change Origin: and Host: headers to match target:
+// 			changeOrigin: true,
+// 			changeHost: true
+// 		}
+// 	];
+// };
+
+import asyncPlugin from 'preact-cli-plugin-async';
+
+export default (config) => {
+	asyncPlugin(config);
 };
