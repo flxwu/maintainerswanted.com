c8d3a0110795dabe99a4b2ba2542c59cd300e7f6
Quentin Oschatz <quentin@cliffhouse.de>
2018-08-31 14:20:49 +0200

    Added Passport and Github OAuth Strategy

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+var passport = require('passport');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
+
+const GitHubStrategy = require('passport-github').Strategy;
 
 var projectRouter = require('./routes/project');
 
@@ -16,6 +19,19 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use('/api/project', projectRouter);
+
+// Configure Github OAuth
+passport.use(new GitHubStrategy({
+	clientID: '2ec1ebabf5ae9436245e',
+	clientSecret: config.GithubSecret,
+	callbackURL: 'http://poll.io.flxwu.com/auth/github/callback' //TODO: Change localhost to production host
+},
+(accessToken, refreshToken, profile, callback) => {
+	User.findOrCreate({ githubId: profile.id }, function (err, user) {
+		return callback(err, user);
+	});
+}
+));
 
 // catch 404
 app.use(function(req, res, next) {
