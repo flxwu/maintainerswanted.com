0a0d573dfd685aa4d89e464cb739de15e4949a4c
Felix Wu <mail@flxwu.com>
2018-08-31 16:56:47 +0200

    add title to icons

--- client/src/components/Icon.js
+++ client/src/components/Icon.js
@@ -3,7 +3,7 @@ import React from 'react';
 const IconURI = title => `../assets/icons/${title}.svg`;
 
 const Icon = props => (
-	<img src={IconURI(props.type)} width={props.width || 50} height={props.height || 50} />
+	<img src={IconURI(props.type)} width={props.width || 50} height={props.height || 50} {...props} />
 );
 
 export default Icon;
