113316b90958c751d6aa3a84d67cf77a4dc28734
Felix Wu <mail@flxwu.com>
2018-08-31 18:12:32 +0200

    Added Firebase storage to project endpoints

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var bodyParser = require('body-parser');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var projectRouter = require('./routes/project');
+var projectListRouter = require('./routes/projectList');
+var userRouter = require('./routes/user');
 
 var app = express();
 
