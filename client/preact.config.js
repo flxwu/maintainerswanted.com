03a907643b66d56b7204f1b566710f6e0e9841a3
Felix Wu <mail@flxwu.com>
2018-09-01 14:21:05 +0200

    make config dev-friendly

--- client/preact.config.js
+++ client/preact.config.js
@@ -1,21 +1,22 @@
-// module.exports = function(config) {
-// 	config.devServer.proxy = [
-// 		{
-// 			// proxy requests matching a pattern:
-// 			path: '/api/**',
-
-// 			// where to proxy to:
-// 			target: 'http://localhost:5000',
-
-// 			// optionally change Origin: and Host: headers to match target:
-// 			changeOrigin: true,
-// 			changeHost: true
-// 		}
-// 	];
-// };
-
 import asyncPlugin from 'preact-cli-plugin-async';
 
 export default (config) => {
-	asyncPlugin(config);
+	if (config.devServer) {
+		config.devServer.proxy = [
+			{
+				// proxy requests matching a pattern:
+				path: '/api/**',
+  
+				// where to proxy to:
+				target: 'http://localhost:5000',
+  
+				// optionally change Origin: and Host: headers to match target:
+				changeOrigin: true,
+				changeHost: true
+			}
+		];
+	}
+	else {
+		asyncPlugin(config);
+	}
 };
