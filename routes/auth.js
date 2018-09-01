58e03d62121b3d3f530aa41fbb4268ba6d1edb07
Felix Wu <mail@flxwu.com>
2018-09-02 01:26:13 +0200

    add auth route

---
+++ routes/auth.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+
+let passport = null;
+
+function getRouter(passportRef) {
+	passport = passportRef;
+	return router;
+}
+
+/**
+ * GET Github Auth
+ */
+router.get('/github', (req, res, next) => {
+  passport.authenticate('github');
+});
+
+/**
+ * GET Github Auth Callback
+ */
+router.get('/github/callback', (req, res, next) => {
+  passport.authenticate('github', {
+		successRedirect: '/',
+		failureRedirect: '/'
+	})
+});
+
+module.exports = getRouter;
