b4fc847904a7090f0d13c1d606f73e325f447d8f
Quentin Oschatz <quentin@cliffhouse.de>
2018-09-01 12:55:18 +0200

    Added Github Authentication

--- app.js
+++ app.js
@@ -9,8 +9,15 @@ var bodyParser = require('body-parser');
 const GitHubStrategy = require('passport-github').Strategy;
 
 var projectRouter = require('./routes/project');
+var github = require('./routes/auth/github');
+var githubcallback = require('./routes/auth/githubcallback');
 
 var app = express();
+
+// Routes
+app.use('/api/project', projectRouter);
+app.use('/api/auth/github', github);
+app.use('/api/auth/guthub/callback', githubcallback);
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -18,20 +25,30 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.use('/api/project', projectRouter);
+// Init passport
+app.use(passport.initialize());
+
+// Passport user (de)-serialisation to avoid transmitting user info after login
+passport.serializeUser(function(user, callback) {
+	callback(null, user);
+});
+
+passport.deserializeUser(function(obj, callback) {
+	callback(null, obj);
+});
 
 // Configure Github OAuth
-// passport.use(new GitHubStrategy({
-// 	clientID: '2ec1ebabf5ae9436245e',
-// 	clientSecret: config.GithubSecret,
-// 	callbackURL: 'http://poll.io.flxwu.com/auth/github/callback' //TODO: Change localhost to production host
-// },
-// (accessToken, refreshToken, profile, callback) => {
-// 	User.findOrCreate({ githubId: profile.id }, function (err, user) {
-// 		return callback(err, user);
-// 	});
-// }
-// ));
+passport.use(new GitHubStrategy({
+	clientID: process.env.GH_KEY,
+	clientSecret: process.env.GH_SECRET,
+	callbackURL: 'localhost:5000/auth/github/callback' //TODO: Change localhost to production host
+},
+(accessToken, refreshToken, profile, callback) => {
+	User.findOrCreate({ githubId: profile.id }, function (err, user) {
+		return callback(err, user);
+	});
+}
+));
 
 // catch 404
 app.use(function(req, res, next) {
