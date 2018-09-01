b769a174de786c6516ac468a7677d9a4c4394b7a
Felix Wu <mail@felixwu.dev>
2018-09-01 19:53:37 +0200

    put preact app.js in src root

--- client/src/app.js
+++ client/src/app.js
@@ -3,97 +3,102 @@ import styled from 'styled-components';
 import axios from 'axios';
 import MediaQuery from 'react-responsive';
 
-import ProjectCard from './project/ProjectCard';
-import NewProject from './form/NewProject';
-import Header from './Header';
-import Loading from './Loading';
+import ProjectCard from './components/project/ProjectCard';
+import NewProject from './components/form/NewProject';
+import Header from './components/Header';
+import Loading from './components/Loading';
 
 export default class App extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			projects: []
+      projects: [],
+      loggedIn: false,
 		};
 
 		// accessibility: add outline if tab is used
 		if (typeof window !== 'undefined') {
-			const handleFirstTab = (e) => {
+			const handleFirstTab = e => {
 				if (e.keyCode === 9) {
 					document.body.classList.add('user-is-tabbing');
 					window.removeEventListener('keydown', handleFirstTab);
 				}
 			};
-    
+
 			window.addEventListener('keydown', handleFirstTab);
 		}
-    
+
 		if (typeof document !== 'undefined') {
 			document.title = 'Maintainers Wanted';
 		}
-    
 	}
 
 	async componentDidMount() {
+    // get if logged in
+    const loggedIn = await axios.get('/api/auth/status');
+
 		// get projects list
 		const response = await axios.get('/api/project/getList');
 		const dataObject = response.data.data[0];
-		const data = Object.keys(dataObject)
-			.map(dbKey => dataObject[dbKey]);
-    
-		this.setState({ projects: data });
+		const data = Object.keys(dataObject).map(dbKey => dataObject[dbKey]);
+
+		this.setState({ projects: data, loggedIn: loggedIn ? true : false });
 	}
 
-	render() {
-		const { projects } = this.state;
+	render({}, { projects, loggedIn }) {
 		return (
 			<div>
 				<Header />
 				<MediaQuery minDeviceWidth={1224}>
 					<List>
-						<NewProject />
-						{projects.length !== 0 ?
+						<NewProject loggedIn={loggedIn}/>
+						{projects.length !== 0 ? (
 							projects.map(project => (
 								<ProjectCardWrapper>
 									<ProjectCard project={project} />
-								</ProjectCardWrapper>)
-							) :
+								</ProjectCardWrapper>
+							))
+						) : (
 							<Loading />
-						}
+						)}
 					</List>
 				</MediaQuery>
 				<MediaQuery maxDeviceWidth={1224}>
 					<List mobile>
-						<NewProject />
-						{projects.length !== 0 ?
+						<NewProject loggedIn={loggedIn} />
+						{projects.length !== 0 ? (
 							projects.map(project => (
 								<ProjectCardWrapper>
 									<ProjectCard project={project} />
-								</ProjectCardWrapper>)
-							) :
+								</ProjectCardWrapper>
+							))
+						) : (
 							<Loading />
-						}
+						)}
 					</List>
 				</MediaQuery>
 				<FooterWrapper>
 					<Footer>
-            Made with love by
+						Made with love by
 						<Link href="https://twitter.com/flxwu"> @flxwu</Link> and
 						<Link href="https://twitter.com/QuentinOschatz"> @Qo2770</Link>
 						<br />
 						<Footer break>
 							<Link break href="https://github.com/flxwu/maintainerswanted.com">
-              Find us on Github!
+								Find us on Github!
 							</Link>
 						</Footer>
 						<br />
 						<Footer small break>
-            Built using
+							Built using
 							<Link small href="https://github.com/flxwu/maintainerswanted.com">
 								{' Preact'}
-							</Link> +
+							</Link>{' '}
+							+
 							<Link small href="https://github.com/flxwu/maintainerswanted.com">
 								{' Express'}
-							</Link> +
+							</Link>{' '}
+							+
 							<Link small href="https://github.com/flxwu/maintainerswanted.com">
 								{' Firebase'}
 							</Link>
@@ -106,47 +111,47 @@ export default class App extends Component {
 }
 
 const List = styled.ul`
-  display: block;
-  margin-block-start: 3em;
-  margin-block-end: 1em;
-  margin-inline-start: 0px;
-  margin-inline-end: 0px;
-  padding-inline-start: ${props => props.mobile ? '10%' : '20%'};
-  -webkit-padding-start: ${props => props.mobile ? '10%' : '20%'};
-  padding-inline-end: ${props => props.mobile ? '10%' : '20%'};
-  -webkit-padding-end: ${props => props.mobile ? '10%' : '20%'};
-  list-style-type: none;
-  background: #FAFAFA;
-  text-align: center;
+	display: block;
+	margin-block-start: 3em;
+	margin-block-end: 1em;
+	margin-inline-start: 0px;
+	margin-inline-end: 0px;
+	padding-inline-start: ${props => (props.mobile ? '10%' : '20%')};
+	-webkit-padding-start: ${props => (props.mobile ? '10%' : '20%')};
+	padding-inline-end: ${props => (props.mobile ? '10%' : '20%')};
+	-webkit-padding-end: ${props => (props.mobile ? '10%' : '20%')};
+	list-style-type: none;
+	background: #fafafa;
+	text-align: center;
 `;
 
 const FooterWrapper = styled.div`
-  display: flex;
-  justify-content: center;
-  background: #FAFAFA;
-  height: 20px;
-  margin: 50px 0;
+	display: flex;
+	justify-content: center;
+	background: #fafafa;
+	height: 20px;
+	margin: 50px 0;
 `;
 
 const Footer = styled.div`
-  position: relative;
-  text-align: center;
-  ${props => props.small && 'font-size: 12px'};
-  ${props => props.break && 'margin: 10px 0'};
+	position: relative;
+	text-align: center;
+	${props => props.small && 'font-size: 12px'};
+	${props => props.break && 'margin: 10px 0'};
 `;
 
 const ProjectCardWrapper = styled.li`
-  position: relative;
-  flex: 1;
+	position: relative;
+	flex: 1;
 `;
 
 const Link = styled.a`
-  text-decoration: none;
-  font-weight: bold;
-  text-align: center;
-  color: #E27D60;
-  &:hover { 
-    color: grey; 
-  };
-  ${props => props.break && 'margin-top: 10px'};
+	text-decoration: none;
+	font-weight: bold;
+	text-align: center;
+	color: #e27d60;
+	&:hover {
+		color: grey;
+	}
+	${props => props.break && 'margin-top: 10px'};
 `;
