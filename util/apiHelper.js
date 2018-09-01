76bc28834d662fbe84ae543fbb7d1944ce298a88
Felix Wu <mail@felixwu.dev>
2018-09-01 14:21:56 +0200

    Fix octokit oauth - enable oauth rate limit

--- util/apiHelper.js
+++ util/apiHelper.js
@@ -1,9 +1,4 @@
-const octokit = require('@octokit/rest')();
-var firebase = require('firebase');
-
-const config = require('./config');
-
-const paginate = async (method, params) => {
+const paginate = async (octokit, method, params) => {
 	let response = await method({ per_page: 100, ...params });
 	let { data } = response;
 	while (octokit.hasNextPage(response)) {
@@ -11,15 +6,6 @@ const paginate = async (method, params) => {
 		data = data.concat(response.data);
 	}
 	return data;
-}
-
-const setup = () => {
-	firebase.initializeApp(config);
-	octokit.authenticate({
-		type: 'oauth',
-		key: process.env.GH_KEY,
-		secret: process.env.GH_SECRET
-	});
 }
 
 // firebase callback after push finished
@@ -31,4 +17,4 @@ const finished = (err) => {
 	}
 }
 
-module.exports = { paginate, setup, finished };
+module.exports = { paginate, finished };
