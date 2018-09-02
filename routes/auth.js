8e442914b395655d6ede7a4f6afb1de826dc2b46
Felix Wu <mail@flxwu.com>
2018-09-02 18:32:38 +0200

    store auth status in session cookie

--- routes/auth.js
+++ routes/auth.js
@@ -4,15 +4,30 @@ const router = express.Router();
 let passport = null;
 
 const getRouter = (passportRef) => {
-	passport = passportRef;
-	return router;
+  passport = passportRef;
+  return router;
 }
+
+/**
+ * GET Auth Status
+ */
+router.get('/status', (req, res, next) => {
+  return res.json({ loggedIn: req.session.loggedIn });
+});
 
 /**
  * GET Github Auth
  */
 router.get('/github', (req, res, next) => {
-  passport.authenticate('github');
+  passport.authenticate('github', (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return res.redirect('/');
+    }
+    req.session.loggedIn = true;
+  })(req, res, next);
 });
 
 /**
@@ -20,9 +35,10 @@ router.get('/github', (req, res, next) => {
  */
 router.get('/github/callback', (req, res, next) => {
   passport.authenticate('github', {
-		successRedirect: '/',
-		failureRedirect: '/'
-	})
+    successRedirect: '/',
+    failureRedirect: '/'
+  })
+  return next();
 });
 
 module.exports = getRouter;
