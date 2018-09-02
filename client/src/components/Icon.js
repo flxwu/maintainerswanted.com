f99b49821534c5657ccd2ce7ab8c99573fddb0eb
Felix Wu <mail@felixwu.dev>
2018-09-02 21:04:44 +0200

    get icons from absolute path

--- client/src/components/Icon.js
+++ client/src/components/Icon.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const IconURI = title => `../assets/icons/${title}.svg`;
+const IconURI = title => `/assets/icons/${title}.svg`;
 
 const Icon = props => (
 	<img src={IconURI(props.type)} width={props.width || 50} height={props.height || 50} {...props} />
