1a121828e2beec56822828aec0a5b5b94ad1ab44
Felix Wu <mail@felixwu.dev>
2018-09-07 21:27:50 +0200

    fix issueTemplate body

--- util/issueTemplate.js
+++ util/issueTemplate.js
@@ -1,11 +1,10 @@
 const issueTemplate = (repo, twitter) => ({
   title: repo + ' is searching for Maintainers!',
-  body: `*This issue was created by [Maintainers Wanted](https://maintainerswanted.com)* :nerd_face: \n
-    *Support us by leaving a star on [Github](https://github.com/flxwu/maintainerswanted.com)!* :star2: \n
-    \n\n ## ${repo} is searching for new Maintainers! :coder: :mailbox-with-mail:\n
-    Do you use ${repo} personally or at work and would like this project to be further developed and improved?\n
-    Or are you already a contributor and ready to take the next step to becoming a maintainer?\n\n
-    ${
+  body: `*This issue was created by [Maintainers Wanted](https://maintainerswanted.com)* :nerd_face: \n\n
+*Support us by leaving a star on [Github](https://github.com/flxwu/maintainerswanted.com)!* :star2: \n\n
+## ${repo} is searching for new Maintainers! :man_technologist: :mailbox_with_mail:\n
+Do you use ${repo} personally or at work and would like this project to be further developed and improved?\n\n
+Or are you already a contributor and ready to take the next step to becoming a maintainer?\n\n${
   twitter
     ? `If you are interested, comment here below on this issue :point_down: or drop me a message on [Twitter](https://twitter.com/${twitter})! :raised_hands:`
     : 'If you are interested, comment here below on this issue! :point_down::raised_hands:'
