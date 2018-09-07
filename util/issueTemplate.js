a5023ec8ae30bf53fd34f578a4881a068e646cf4
Felix Wu <mail@flxwu.com>
2018-09-07 21:16:58 +0200

    fix issue Template body

--- util/issueTemplate.js
+++ util/issueTemplate.js
@@ -1,15 +1,15 @@
 const issueTemplate = (repo, twitter) => ({
-  title: repo + ' searches for Maintainers!',
-  body:
-    '*This issue was created by [Maintainers Wanted](https://maintainerswanted.com)* :nerd-face: \n' +
-    '*Support us by leaving a star on [Github!](https://github.com/flxwu/maintainerswanted.com)* :star2: \n' +
-    `\n\n ## ${repo} is searching for new Maintainers! :coder: :mailbox-with-mail:\n` +
-    ` Do you use ${repo} personally or at work and would like this project to be further developed and improved?\n` +
-    ' Or are you already a contributor and ready to take the next step to becoming a maintainer?\n\n' +
-    twitter
-      ? `If you are interested, comment here below on this issue :point-down or
-        ${' '}drop me a message on [Twitter](https://twitter.com/${twitter})! :raised-hands:`
-      : 'If you are interested, comment here below on this issue! :point-down::raised-hands:',
+  title: repo + ' is searching for Maintainers!',
+  body: `*This issue was created by [Maintainers Wanted](https://maintainerswanted.com)* :nerd_face: \n
+    *Support us by leaving a star on [Github](https://github.com/flxwu/maintainerswanted.com)!* :star2: \n
+    \n\n ## ${repo} is searching for new Maintainers! :coder: :mailbox-with-mail:\n
+    Do you use ${repo} personally or at work and would like this project to be further developed and improved?\n
+    Or are you already a contributor and ready to take the next step to becoming a maintainer?\n\n
+    ${
+  twitter
+    ? `If you are interested, comment here below on this issue :point_down: or drop me a message on [Twitter](https://twitter.com/${twitter})! :raised_hands:`
+    : 'If you are interested, comment here below on this issue! :point_down::raised_hands:'
+  }`,
   comment:
     'This issue got closed by the project\'s maintainers. ' +
     'Please don\'t reopen this issue but instead readd the project on [Maintainers Wanted](https://maintainerswanted.com)' +
